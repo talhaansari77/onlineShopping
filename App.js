c7165335a244230e394600dfd3b3d667d383365a
talhaansari77 <talhaprince09@gmail.com>
2022-08-08 23:42:27 -0700

    Genos

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <Text>This iS Online Shopping </Text>
+    </View>
+  );
+};
+
+export default App;
